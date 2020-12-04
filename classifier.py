74e1f452308f88c14f0372998dc38d7e195c7280
Erfan Arefi <marefimoghaddam@unixerr.com>
2020-12-05 02:02:25 +0330

    calculate mean and std for dataset then normalize the dataset using those mean and std

--- classifier.py
+++ classifier.py
@@ -1,15 +1,37 @@
 
 
 
+# coding: utf-8
+
+'''
+	Codded By : 
+ █     █░ ██▓ ██▓    ▓█████▄  ▒█████   ███▄    █  ██▓ ▒█████   ███▄    █ 
+▓█░ █ ░█░▓██▒▓██▒    ▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒█░ █ ░█ ▒██▒▒██░    ░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░█░ █ ░█ ░██░▒██░    ░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░██░▒██   ██░▓██▒  ▐▌██▒
+░░██▒██▓ ░██░░██████▒░▒████▓ ░ ████▓▒░▒██░   ▓██░░██░░ ████▓▒░▒██░   ▓██
+-------------------------------------------------------------------------------------------------
+| Feature Selection and Dimensionality Reduction using Genetic Algorithm For Breast Cancer Dataset
+|-------------------------------------------------------------------------------------------------
+|
+| USAGE : _______training_______ 
+|			python classifier.py --network mlp --batch-size 32 --num-workers 4 --epoch 200 --learning-rate 0.001 --device cpu
+|
+|
+| USAGE : _______predicting using mlp model_______
+|			python classifier.py --pre-trained-model path/to/mlp.pth
+|
+| 
+| USAGE : _______predicting using cnn model_______
+| 			python classifier.py --pre-trained-model path/to/cnn.pth
+|
+|
+|
+|
 
 
-# TODO : complete readme
-# TODO : add mean and std for persian database
+'''
 
-
-
-# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
-# https://github.com/khshim/pytorch_mnist/
 
 
 import time
@@ -23,7 +45,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import torch.optim as optim
 from dataset import PersianAlphabetDataset
-from utils import ToTensor, Normalize, UnNormalize, Img2CSV
+from utils import ToTensor, Normalize, UnNormalize, CalMeanStd0
 from model import CNN, MLP
 
 
@@ -35,11 +57,11 @@ parser.add_argument('--batch-size', action='store', type=int, help='The number o
 parser.add_argument('--num-workers', action='store', type=int, help='The number of workers for dataloader object')
 parser.add_argument('--epoch', action='store', type=int, help='The number of epochs')
 parser.add_argument('--learning-rate', action='store', type=float, help='Learning rate value')
-parser.add_argument('--image-size', action='store', type=int, help='The width and height size of image')
-parser.add_argument('--classes', action='store', type=int, help='The number of total classes for network output')
 parser.add_argument('--pre-trained-model', action='store', type=str, help='Path to pre-trained model')
 parser.add_argument('--device', action='store', type=str, help='The device to attach the torch to')
 args = parser.parse_args()
+
+
 
 
 
@@ -48,6 +70,9 @@ args = parser.parse_args()
 cuda = torch.cuda.is_available() if args.device == 'cuda' else None
 device = torch.device("cuda" if cuda else "cpu")
 torch.backends.cudnn.benchmark = True
+
+
+
 
 
 
@@ -61,19 +86,29 @@ def get_sample(dataloader):
 			break
 	return inputs, labels
 
-
 def train(model, optimizer, loader):
-	# model.train()
-	# output = net(Variable(data.to(device)))
-	pass
+	model.train()
+	for e in range(args.epoch):
+		for idx, sample in enumerate(loader):
+			images, labels = sample
+			images = Variable(images.to(device))
+			if model.__class__.__name__ == "MLP":
+				pass
+			elif model.__class__.__name__ == "CNN":
+				pass
+			else:
+				print("[?] Can't Train Unsupported Network")
 
 def evaluate(model, loader):
 	pass
 
 
 
-# ------------ load pre-trained model
-# ---------------------------------------
+
+
+
+# ------------ load pre-trained model for predicting 
+# -----------------------------------------------------
 if args.pre_trained_model and os.path.exists(args.pre_trained_model):
 	print(f"\n________found existing pre-trained model, start testing________\n")
 	try:
@@ -83,15 +118,15 @@ if args.pre_trained_model and os.path.exists(args.pre_trained_model):
 		print(f"\t➢   can't load pre-trained model from : {args.pre_trained_model}\n")
 
 	if args.pre_trained_model.split("/")[1][:-3] == "mlp":
-		# TODO
-		# load mlp model
+		# TODO - load mlp model
 		# start testing using mlp model
 		pass
 	elif args.pre_trained_model.split("/")[1][:-3] == "cnn":
-		# TODO
-		# load cnn model
+		# TODO - load cnn model
 		# start testing using cnn model
 		pass
+
+
 
 
 
@@ -100,105 +135,85 @@ if args.pre_trained_model and os.path.exists(args.pre_trained_model):
 # ----------------------------------------------------------------------------------------------
 else:
 	print(f"\n________found no existing pre-trained model, start training________\n")
-	
-
-	# --------------------- building model
-	# ---------------------------------------------
-	if args.network == "mlp":
-		net = MLP(input_neurons=args.image_size**2, output_neurons=args.classes, learning_rate=args.learning_rate)
-	elif args.network == "cnn":
-		net = CNN()
-	else:
-		print("[?] Not Supported Network!")
-		sys.exit(1)
 
 
 
-	# --------------------- preparing dataset
-	# ---------------------------------------------
-	transform = transforms.Compose([ToTensor(), Normalize(mean=[13.4820], std=[49.6567])]) # normalize image per channel - passing one value for std and mean cause we have one channel
+
+	# 					   ------------------------------------------
+	# --------------------- calculating mean and std of training data
+	# 					   ------------------------------------------
+	# 
+	# we do not have to pass the ToTensor an Normalize transforms to 
+	# PersianAlphabetDataset cause Dataloader will turn images of dataset
+	# into tensor and since we're calculating the mean and std there is no
+	# need to pass Normalize in here!
+	# -----------------------------------------------------------------------
+	cal_mean_std_iter = DataLoader(PersianAlphabetDataset(csv_files=['dataset/train_x.csv', 'dataset/train_y.csv']), batch_size=args.batch_size)
+	mean, std = CalMeanStd0(cal_mean_std_iter) # you have to pass a dataloader object
+
+
+
+
+
+	# 					   ------------------
+	# --------------------- building dataset
+	# 					   ------------------
+	# 
+	# normalize image using calculated per channel mean and std
+	# passing one value for std and mean cause we have one channel
+	# --------------------------------------------------------------------
+	transform = transforms.Compose([ToTensor(), Normalize(mean=mean, std=std)])
 	training_transformed = PersianAlphabetDataset(csv_files=['dataset/train_x.csv', 'dataset/train_y.csv'], transform=transform)
 	valid_transformed  = PersianAlphabetDataset(csv_files=['dataset/test_x.csv', 'dataset/test_y.csv'], transform=transform)
 
 
 
 
+
 	# --------------------- building dataloader objects
-	# -----------------------------------------------------
+	# -----------------------------------------------------	
 	train_iter = DataLoader(training_transformed, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 	valid_iter  = DataLoader(valid_transformed, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
 	
 
-
-	#						-----------------------------------------
-	# --------------------- calculating mean and std of training data
-	# 						-----------------------------------------
-	# 
-	# https://discuss.pytorch.org/t/computing-the-mean-and-std-of-dataset/34949/2
-	# https://stackoverflow.com/questions/60485362/how-to-normalize-images-in-pytorch
-	# ------------------------------------------------------------------------------------
 	
-
-							# ========================
-							#  SOLUTION 1
-							# ========================
-
-	# mean = 0.
-	# std = 0.
-	# nb_samples = 0.
-	# for data in train_iter:
-	# 	batch_samples = data[0].size(0)
-	# 	data = data[0].view(batch_samples, data[0].size(1), -1)
-	# 	mean += data.mean(2).sum(0)
-	# 	std += data.std(2).sum(0)
-	# 	nb_samples += batch_samples
-
-	# mean /= nb_samples
-	# std /= nb_samples
-
-	# print(mean)
-	# print(std)
-	
-
-							# ========================
-							#  SOLUTION 2
-							# ========================
-
-	# cnt = 0
-	# fst_moment = torch.empty(3)
-	# snd_moment = torch.empty(3)
-
-	# for _, sample in enumerate(train_iter):
-	# 	images, _ = sample
-	# 	b, c, h, w = images.shape
-	# 	nb_pixels = b * h * w
-	# 	sum_ = torch.sum(images, dim=(0, 2, 3))
-	# 	sum_of_square = torch.sum(images**2, dim=(0, 2, 3))
-	# 	fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
-	# 	snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
-	# 	cnt += nb_pixels
-
-	# print("mean", fst_moment)
-	# print("std", torch.sqrt(snd_moment - fst_moment ** 2))
-
-
 
 	# --------------------- getting a sample
 	# ---------------------------------------------
-	sample = get_sample(train_iter)
-	sample_inputs = sample[0]
-	sample_labels = sample[1]
+	mini_batch = get_sample(train_iter)
+	mini_batch_inputs = mini_batch[0]
+	mini_batch_labels = mini_batch[1]
 
+	
+	
 
 	# --------------------- plottig a sample
 	# ---------------------------------------------
 	plt.figure()
-	plt.imshow(sample_inputs[0].permute(1, 2, 0).numpy())
+	plt.imshow(mini_batch_inputs[0].permute(1, 2, 0).numpy())
 	plt.show()
+
+
+	
+
+
+	# --------------------- building model
+	# ---------------------------------------------
+	if args.network == "mlp":
+		net = MLP(input_neurons=mini_batch_inputs.shape[2]**2, output_neurons=mini_batch_labels.shape[1], learning_rate=args.learning_rate)
+	elif args.network == "cnn":
+		net = CNN(input_channels=mini_batch_inputs.shape[1], output_neurons=mini_batch_labels.shape[1])
+	else:
+		print("[?] Not Supported Network!")
+		sys.exit(1)
+
+
 
 
 	# --------------------- training process
 	# ---------------------------------------------
 	optimizer = optim.Adam(net.parameters(), args.learning_rate)
-	train(net.to(device), optimizer, train_iter) # training model process
+	# train(net.to(device), optimizer, train_iter) # training model process
+	# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+	# https://github.com/khshim/pytorch_mnist/
